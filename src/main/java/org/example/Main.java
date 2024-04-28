7e57039316ec3253dde91cd007014c45ad382a42
Андрій Просвітлюк <prosvitlyukandrew@gmail.com>
2024-04-28 12:02:58 +0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,43 @@
+package org.example;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Введите число: ");
+            int value = scanner.nextInt();
+            if (i==0){ arr[i] = value;}
+            for (int j = 0; j < i; j++) {
+                if (value == arr[j]) {
+                    while (value == arr[j]) {
+                        System.out.println("Таке число вже є,введите повторно число: ");
+                        value = scanner.nextInt();
+                        arr[i] = value;
+                    }}else{ arr[i] = value;}
+            }}
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("число: " + arr[i]);
+        }
+        System.out.println();
+        boolean fal = false;
+        int number;
+        while (!fal) {
+            fal = true;
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    fal = false;
+
+                    number = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = number;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("число: " + arr[i]);
+        }
+    }
+}
